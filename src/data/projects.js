0044d0a71d3f6ff75b7d74ced9ee4f7931d17207
Harshad <56105291+Harshad165@users.noreply.github.com>
2021-02-05 11:22:44 +0530

    initial commit

---
+++ src/data/projects.js
@@ -0,0 +1,50 @@
+// TODO Add a couple lines about each project
+const data = [
+  {
+    title: 'Socio',
+    subtitle: '2019 December',
+    link: 'https://github.com/Tesla-Atoz/Social_Post_website',
+    image: '/images/projects/Social_Post_website.png',
+    date: '2019-9-1',
+    desc:
+      'A web application where a user can create a account, login, create, delete posts. It also enhances user to create groups and join other groups. It is basically authentication of user and performing CRUD operations over data.',
+  },
+  {
+    title: 'News Aggregator with voice assistant',
+    subtitle: 'Alan-Ai and ReactJs',
+    link: 'https://react-alan-ai.netlify.app',
+    image: '/images/projects/Alan-Ai.png',
+    date: '2019-12-1',
+    desc:
+      'A web application where it collects and displays top news articles from various resources by categories (ex: Business, Entertainment), Terms (ex : Bitcoin, Smartphones) and also by sources (ex: CNN, BBC News). Navigation is enabled through a incredible voice assistant ',
+  },
+  {
+    title: 'Customize Burger',
+    subtitle: 'Are you hungry? Just make a burger and place a order',
+    link: 'https://github.com/Tesla-Atoz/Burger_project',
+    image: '/images/projects/burger.png',
+    date: '2020-4-2',
+    desc:
+     'A web application where one can customize a burger themselves, place a order and check-out.(ex: A burger with 2cheese slices, bacon, 2 veg nuggets) ',
+  },
+  {
+    title: 'Artificial Neural Networks (Classification)',
+    subtitle: 'Predicts whether a bank customer leaves the bank or not',
+    link: 'https://github.com/Tesla-Atoz/artificialneuralnetwork',
+    image: '/images/projects/interactive gif.gif',
+    date: '2021-2-4',
+    desc:
+     'A deep learning project where the model predicts whether a bank customer exits or not based on his/her previous data  ',
+  },
+  {
+    title: 'Artificial Neural Networks (Regression)',
+    subtitle: 'Predict power values',
+    link: 'https://github.com/Tesla-Atoz/neuralNetworkForRegressionProblem',
+    image: '/images/projects/all_deviations.png',
+    date: '2021-2-2',
+    desc:
+     'A deep learning model for predicting power consumption. This is regression problem. Detailed with comments. ',
+  },
+];
+
+export default data;
